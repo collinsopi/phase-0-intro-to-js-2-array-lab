116196ed03086f19f06f21acc3cd5c8c9662ca7c
Collins Opiayo <opiayocollins2@gmail.com>
2025-06-08 19:58:38 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+function destructivelyRemoveLastCat(){
+cats.pop();
+return cats;
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+const appendCat = (name) => [...cats, name];
+const prependCat = function(name) {
+  return [name, ...cats];
+};
+const removeLastCat = function() {
+  return cats.slice(0, -1);
+};
+const removeFirstCat = function() {
+  return cats.slice(1);
+};
+
